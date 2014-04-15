29de8bec3ec20d8aca326a8b944a35bb23e6fef9
farry19 <farrukhayyaz@gmail.com>
2014-04-15 08:50:17 +0300

    Web Service + Wireless Communication

---
+++ README.txt
@@ -0,0 +1,1 @@
+ANDROID Home Automation
---
+++ src/com/akz/smarth/LoginActivity.java
@@ -0,0 +1,105 @@
+/**
+ * Author: Abdul Karim Zahrani
+ * URL: none
+ * twitter: http://twitter.com/akzahrani
+ * */
+package com.akz.smarth;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.akz.smarth.library.DatabaseHandler;
+import com.akz.smarth.library.UserFunctions;
+
+public class LoginActivity extends Activity {
+	Button btnLogin;
+//	Button btnLinkToRegister;
+	EditText inputEmail;
+	EditText inputPassword;
+	TextView loginErrorMsg, response;
+
+	// JSON Response node names
+	private static String KEY_SUCCESS = "success";
+	private static String KEY_ERROR = "error";
+	private static String KEY_ERROR_MSG = "error_msg";
+	private static String KEY_UID = "uid";
+	private static String KEY_NAME = "name";
+	private static String KEY_EMAIL = "email";
+	private static String KEY_CREATED_AT = "created_at";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+		.detectAll()
+		.penaltyLog()
+		.build();
+		
+		StrictMode.setThreadPolicy(policy);
+		
+		setContentView(R.layout.login);
+
+		// Importing all assets like buttons, text fields
+		inputEmail = (EditText) findViewById(R.id.loginEmail);
+		inputPassword = (EditText) findViewById(R.id.loginPassword);
+		btnLogin = (Button) findViewById(R.id.btnLogin);
+		loginErrorMsg = (TextView) findViewById(R.id.login_error);
+
+		// Login button Click Event
+		btnLogin.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View view) {
+				String email = inputEmail.getText().toString();
+				String password = inputPassword.getText().toString();
+				UserFunctions userFunction = new UserFunctions();
+				Log.d("Button", "Login");
+				JSONObject json = userFunction.loginUser(email, password);
+
+				// check for login response
+				try {
+					if (json.getString(KEY_SUCCESS) != null) {
+						loginErrorMsg.setText("");
+						String res = json.getString(KEY_SUCCESS); 
+						if(Integer.parseInt(res) == 1){
+							// user successfully logged in
+							// Store user details in SQLite Database
+							DatabaseHandler db = new DatabaseHandler(getApplicationContext());
+							JSONObject json_user = json.getJSONObject("user");
+							
+							// Clear all previous data in database
+							userFunction.logoutUser(getApplicationContext());
+							db.addUser(json_user.getString(KEY_NAME), json_user.getString(KEY_EMAIL), json.getString(KEY_UID), json_user.getString(KEY_CREATED_AT));						
+							
+							// Launch Dashboard Screen
+							Intent dashboard = new Intent(getApplicationContext(), DashboardActivity.class);
+							
+							// Close all views before launching Dashboard
+							dashboard.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+							startActivity(dashboard);
+							
+							// Close Login Screen
+							finish();
+						}else if(json.getString(KEY_ERROR) != null){
+							// Error in login
+							loginErrorMsg.setText(json.getString(KEY_ERROR_MSG).toString());
+						}
+					}
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+	}
+}
